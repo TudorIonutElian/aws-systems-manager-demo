cac9b3b8792e5db49909725eb16c7130c3ac118c
Ionut Tudor <ionut.tudor@consultant.lego.com>
2026-03-30 11:55:28 +0300

    DEMO - fix table locking

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
         bucket         = "teamcity-ssm-demo"
         key            = "teamcity-ssm-demo-state.tfstate"
         region         = "eu-central-1"
-        dynamodb_table = "teamcity-ssm-demo"
+        use_lockfile   = true
     }
 }
